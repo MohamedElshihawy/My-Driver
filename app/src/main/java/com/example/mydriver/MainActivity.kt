59dfddb7174c9fcff45333f8aacd90ef6f7a42d9
Mohamed Reda El-Shihawy <91369966+MohamedElshihawy@users.noreply.github.com>
2023-07-16 13:18:59 +0300

    login and registration with firebase Auth and realtime database to store user information

---
+++ app/src/main/java/com/example/mydriver/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mydriver
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mydriver.navigation.NavigationGraphSetUp
+import com.example.mydriver.ui.theme.MyDriverTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyDriverTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    NavigationGraphSetUp()
+                }
+            }
+        }
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+        classpath("com.android.tools.build:gradle:8.0.2")
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+}
+true // Needed to make the Suppress annotation work for the plugins block
